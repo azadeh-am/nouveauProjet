bbb6afb8a0554c576cdbaa955cf9bf25168411e1
TLDevW <lalannethomas08@gmail.com>
2021-05-11 15:24:22 +0200

    Rajout fonction sur bouton

--- nouveauProjet.js
+++ nouveauProjet.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
-}
+    document.getElementById('bouton').addEventListener('click', function () {
+        alert('Ca fonctionne');
+    })
+})
