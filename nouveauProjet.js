77b0cd5a97dfd9abe2586f9982a413750c85317a
Azadeh <azadeh.amohammadi@gmail.com>
2021-05-11 20:47:53 +0200

    une petite correction

--- nouveauProjet.js
+++ nouveauProjet.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
+    var input = document.getElementById("input")
     document.getElementById('bouton').addEventListener('click', function () {
-        alert('Ca fonctionne');
+        if (input.innerHTML = "") {
+            alert("remplissez les champs")
+        } else {
+            alert('Ca fonctionne');
+        }
     })
 })
