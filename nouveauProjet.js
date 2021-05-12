55530b20fef80472841ab3f340f6078af5099446
@ayyoub36-code <ayyoub36@hotmail.fr>
2021-05-12 10:08:05 +0200

    ajouter modif html et js

--- nouveauProjet.js
+++ nouveauProjet.js
@@ -1,10 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var input = document.getElementById("input")
-    document.getElementById('bouton').addEventListener('click', function () {
-        if (input.innerHTML = "") {
-            alert("remplissez les champs")
-        } else {
-            alert('Ca fonctionne');
-        }
-    })
-})
+document.addEventListener('DOMContentLoaded', function () {
+    let inputs = document.querySelectorAll("input")
+    let button = document.getElementById('bouton')
+    button.addEventListener('click', function () {
+        for (let input of inputs)
+            if (input.value == "") {
+                alert("remplissez les champs");
+            } else {
+                alert("ça fonctionne");
+            }
+    });
+});
